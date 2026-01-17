c56acfc456ccfed1c21f67b3435e543bbc91acbd
Nathan Pawelek <nathan@nathanpawelek.com>
2026-01-17 07:15:02 -0600

    migrate sonarr-anime
    Signed-off-by: Nathan Pawelek <nathan@nathanpawelek.com>

---
+++ terraform/authentik/sonarr-anime.tf
@@ -0,0 +1,23 @@
+resource "authentik_provider_proxy" "sonarr_anime" {
+  name                         = "sonarr-anime-proxy"
+  access_token_validity        = var.access_token_validity
+  authorization_flow           = data.authentik_flow.default_authorization_flow.id
+  invalidation_flow            = data.authentik_flow.default_invalidation_flow.id
+  external_host                = "https://sonarr-anime.${var.domain}"
+  internal_host_ssl_validation = true
+  mode                         = "forward_single"
+  intercept_header_auth        = true
+}
+
+resource "authentik_application" "sonarr_anime" {
+  name               = "Sonarr Anime"
+  slug               = "sonarr-anime"
+  protocol_provider  = authentik_provider_proxy.sonarr_anime.id
+  meta_launch_url    = "https://sonarr-anime.${var.domain}"
+  policy_engine_mode = "any"
+}
+
+resource "authentik_outpost_provider_attachment" "sonarr_anime" {
+  outpost           = data.authentik_outpost.embedded.id
+  protocol_provider = authentik_provider_proxy.sonarr_anime.id
+}
