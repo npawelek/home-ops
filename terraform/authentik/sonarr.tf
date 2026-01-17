e3314a2fa4e8c36eca09527d5e2aaa0f9aa50504
Nathan Pawelek <nathan@nathanpawelek.com>
2026-01-16 22:41:00 -0600

    migrate sonarr
    Signed-off-by: Nathan Pawelek <nathan@nathanpawelek.com>

---
+++ terraform/authentik/sonarr.tf
@@ -0,0 +1,23 @@
+resource "authentik_provider_proxy" "sonarr" {
+  name                         = "sonarr-proxy"
+  access_token_validity        = var.access_token_validity
+  authorization_flow           = data.authentik_flow.default_authorization_flow.id
+  invalidation_flow            = data.authentik_flow.default_invalidation_flow.id
+  external_host                = "https://sonarr.${var.domain}"
+  internal_host_ssl_validation = true
+  mode                         = "forward_single"
+  intercept_header_auth        = true
+}
+
+resource "authentik_application" "sonarr" {
+  name               = "Sonarr"
+  slug               = "sonarr"
+  protocol_provider  = authentik_provider_proxy.sonarr.id
+  meta_launch_url    = "https://sonarr.${var.domain}"
+  policy_engine_mode = "any"
+}
+
+resource "authentik_outpost_provider_attachment" "sonarr" {
+  outpost           = data.authentik_outpost.embedded.id
+  protocol_provider = authentik_provider_proxy.sonarr.id
+}
