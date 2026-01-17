00d1deb173ff178a7290720ab83b0ba60a236767
Nathan Pawelek <nathan@nathanpawelek.com>
2026-01-17 07:03:33 -0600

    migrate sonarr-remux
    Signed-off-by: Nathan Pawelek <nathan@nathanpawelek.com>

---
+++ terraform/authentik/sonarr-remux.tf
@@ -0,0 +1,23 @@
+resource "authentik_provider_proxy" "sonarr_remux" {
+  name                         = "sonarr-remux-proxy"
+  access_token_validity        = var.access_token_validity
+  authorization_flow           = data.authentik_flow.default_authorization_flow.id
+  invalidation_flow            = data.authentik_flow.default_invalidation_flow.id
+  external_host                = "https://sonarr-remux.${var.domain}"
+  internal_host_ssl_validation = true
+  mode                         = "forward_single"
+  intercept_header_auth        = true
+}
+
+resource "authentik_application" "sonarr_remux" {
+  name               = "Sonarr Remux"
+  slug               = "sonarr-remux"
+  protocol_provider  = authentik_provider_proxy.sonarr_remux.id
+  meta_launch_url    = "https://sonarr-remux.${var.domain}"
+  policy_engine_mode = "any"
+}
+
+resource "authentik_outpost_provider_attachment" "sonarr_remux" {
+  outpost           = data.authentik_outpost.embedded.id
+  protocol_provider = authentik_provider_proxy.sonarr_remux.id
+}
