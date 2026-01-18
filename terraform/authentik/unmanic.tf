66b7b569dcac79121c70ce637fa7c50a0ebe33e7
Nathan Pawelek <nathan@nathanpawelek.com>
2026-01-18 01:22:44 -0600

    migrate unmanic
    Signed-off-by: Nathan Pawelek <nathan@nathanpawelek.com>

---
+++ terraform/authentik/unmanic.tf
@@ -0,0 +1,23 @@
+resource "authentik_provider_proxy" "unmanic" {
+  name                         = "unmanic-proxy"
+  access_token_validity        = var.access_token_validity
+  authorization_flow           = data.authentik_flow.default_authorization_flow.id
+  invalidation_flow            = data.authentik_flow.default_invalidation_flow.id
+  external_host                = "https://unmanic.${var.domain}"
+  internal_host_ssl_validation = true
+  mode                         = "forward_single"
+  intercept_header_auth        = true
+}
+
+resource "authentik_application" "unmanic" {
+  name              = "Unmanic"
+  slug              = "unmanic"
+  protocol_provider = authentik_provider_proxy.unmanic.id
+  meta_launch_url   = "https://unmanic.${var.domain}"
+  policy_engine_mode = "any"
+}
+
+resource "authentik_outpost_provider_attachment" "unmanic" {
+  outpost           = data.authentik_outpost.embedded.id
+  protocol_provider = authentik_provider_proxy.unmanic.id
+}
