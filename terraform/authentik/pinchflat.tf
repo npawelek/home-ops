27900177d27fe42f3d2f3a49be66107e684e6bf3
Nathan Pawelek <nathan@nathanpawelek.com>
2026-01-19 00:07:58 -0600

    migrate pinchflat
    Signed-off-by: Nathan Pawelek <nathan@nathanpawelek.com>

---
+++ terraform/authentik/pinchflat.tf
@@ -0,0 +1,23 @@
+resource "authentik_provider_proxy" "pinchflat" {
+  name                         = "pinchflat-proxy"
+  access_token_validity        = var.access_token_validity
+  authorization_flow           = data.authentik_flow.default_authorization_flow.id
+  invalidation_flow            = data.authentik_flow.default_invalidation_flow.id
+  external_host                = "https://pinchflat.${var.domain}"
+  internal_host_ssl_validation = true
+  mode                         = "forward_single"
+  intercept_header_auth        = true
+}
+
+resource "authentik_application" "pinchflat" {
+  name               = "Pinchflat"
+  slug               = "pinchflat"
+  protocol_provider  = authentik_provider_proxy.pinchflat.id
+  meta_launch_url    = "https://pinchflat.${var.domain}"
+  policy_engine_mode = "any"
+}
+
+resource "authentik_outpost_provider_attachment" "pinchflat" {
+  outpost           = data.authentik_outpost.embedded.id
+  protocol_provider = authentik_provider_proxy.pinchflat.id
+}
