9ed7a1ac4d739de243a2d421c809f61c0ed75f48
Nathan Pawelek <nathan@nathanpawelek.com>
2026-03-25 16:46:14 -0500

    add oauth for authentik
    Signed-off-by: Nathan Pawelek <nathan@nathanpawelek.com>

---
+++ terraform/authentik/lubelogger.tf
@@ -0,0 +1,37 @@
+resource "authentik_provider_oauth2" "lubelogger" {
+  name               = "lubelogger-oauth"
+  client_id          = "lubelogger"
+  authorization_flow = data.authentik_flow.default_authorization_flow.id
+  invalidation_flow  = data.authentik_flow.default_invalidation_flow.id
+  property_mappings = [
+    data.authentik_property_mapping_provider_scope.scope_openid.id,
+    data.authentik_property_mapping_provider_scope.scope_profile.id,
+    data.authentik_property_mapping_provider_scope.scope_email.id,
+  ]
+  allowed_redirect_uris = [
+    {
+      matching_mode = "strict"
+      url           = "https://lubelogger.${var.domain}/Login/RemoteAuth"
+    }
+  ]
+  signing_key            = data.authentik_certificate_key_pair.default.id
+  access_token_validity  = var.access_token_validity
+  refresh_token_validity = var.refresh_token_validity
+  client_type            = "confidential"
+  include_claims_in_id_token = true
+}
+
+resource "authentik_application" "lubelogger" {
+  name               = "LubeLogger"
+  slug               = "lubelogger"
+  protocol_provider  = authentik_provider_oauth2.lubelogger.id
+  meta_icon          = "https://raw.githubusercontent.com/loganmarchione/homelab-svg-assets/refs/heads/main/assets/ittools.svg"
+  meta_launch_url    = "https://lubelogger.${var.domain}"
+  policy_engine_mode = "any"
+}
+
+resource "authentik_policy_binding" "lubelogger_admins_access" {
+  target = authentik_application.lubelogger.uuid
+  group  = data.authentik_group.authentik_admins.id
+  order  = 0
+}
