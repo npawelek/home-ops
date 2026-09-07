5e597e43624c2aa8bfa48d363769548e30645bb0
Nathan Pawelek <nathan@nathanpawelek.com>
2026-09-07 08:01:03 -0500

    feat: fully remove minio
    Signed-off-by: Nathan Pawelek <nathan@nathanpawelek.com>

--- terraform/minio/outputs.tf
+++
@@ -1,8 +0,0 @@
-output "buzz_access_key" {
-  value = minio_iam_service_account.buzz.access_key
-}
-
-output "buzz_secret_key" {
-  value     = minio_iam_service_account.buzz.secret_key
-  sensitive = true
-}
