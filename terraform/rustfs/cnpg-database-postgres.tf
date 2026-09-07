2b52f670442748a6722deab355fc47e86f3b6deb
Nathan Pawelek <nathan@nathanpawelek.com>
2026-09-07 11:23:01 -0500

    feat: setup barman buckets in rustfs
    Signed-off-by: Nathan Pawelek <nathan@nathanpawelek.com>

---
+++ terraform/rustfs/cnpg-database-postgres.tf
@@ -0,0 +1,52 @@
+resource "minio_s3_bucket" "cnpg_database_postgres" {
+  bucket = "cnpg-database-postgres"
+  acl    = "private"
+}
+
+resource "minio_iam_user" "cnpg_database_postgres" {
+  name          = "cnpg-database-postgres"
+  force_destroy = true
+}
+
+resource "minio_iam_policy" "cnpg_database_postgres" {
+  name = "cnpg-database-postgres-s3"
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = ["s3:GetBucketLocation", "s3:ListBucket"]
+        Resource = "arn:aws:s3:::cnpg-database-postgres"
+      },
+      {
+        Effect   = "Allow"
+        Action   = ["s3:GetObject", "s3:PutObject", "s3:DeleteObject"]
+        Resource = "arn:aws:s3:::cnpg-database-postgres/*"
+      }
+    ]
+  })
+}
+
+resource "minio_iam_user_policy_attachment" "cnpg_database_postgres" {
+  user_name   = minio_iam_user.cnpg_database_postgres.name
+  policy_name = minio_iam_policy.cnpg_database_postgres.name
+}
+
+resource "minio_iam_service_account" "cnpg_database_postgres" {
+  target_user = minio_iam_user.cnpg_database_postgres.name
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = ["s3:GetBucketLocation", "s3:ListBucket"]
+        Resource = "arn:aws:s3:::cnpg-database-postgres"
+      },
+      {
+        Effect   = "Allow"
+        Action   = ["s3:GetObject", "s3:PutObject", "s3:DeleteObject"]
+        Resource = "arn:aws:s3:::cnpg-database-postgres/*"
+      }
+    ]
+  })
+}
--- terraform/rustfs/outputs.tf
+++ terraform/rustfs/outputs.tf
@@ -6,3 +6,21 @@ output "buzz_secret_key" {
   value     = minio_iam_service_account.buzz.secret_key
   sensitive = true
 }
+
+output "cnpg_database_postgres_access_key" {
+  value = minio_iam_service_account.cnpg_database_postgres.access_key
+}
+
+output "cnpg_database_postgres_secret_key" {
+  value     = minio_iam_service_account.cnpg_database_postgres.secret_key
+  sensitive = true
+}
+
+output "cnpg_authentik_postgres_access_key" {
+  value = minio_iam_service_account.cnpg_authentik_postgres.access_key
+}
+
+output "cnpg_authentik_postgres_secret_key" {
+  value     = minio_iam_service_account.cnpg_authentik_postgres.secret_key
+  sensitive = true
+}
